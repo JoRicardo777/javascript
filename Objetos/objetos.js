9d5ba73ad3891e94f87b85e208acec974c07e1db
Ricardo <joanrimalaleal@gmail.com>
2024-10-03 21:08:56 -0300

    Commit da divida

---
+++ Objetos/objetos.js
@@ -0,0 +1,15 @@
+let xicara = {
+    cor: "azul",
+    forma: "boleada",
+    peso: 6,
+
+}
+
+let fruta = {
+    nome: "Maça",
+    cor: "x",
+    peso: "50 gr",
+    tipo: "Citrica"
+}
+
+console.log(fruta,peso);
